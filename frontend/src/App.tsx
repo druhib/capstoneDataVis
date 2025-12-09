436581f96ef94f74086dab706f93cb9af28cfd3c
druhib <bhargava.d@northeastern.edu>
2025-12-09 16:23:45 -0500

    documentation on App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,13 +23,17 @@ import { Button, Select } from 'antd';
       // steps to enable forwarding port: 
       // a. Ports -> Forward a Port
       // b. Type in 5173 
-      // c. Change Visibility from Private to public 
+      // c. Change visibility from private to public 
       // change url below so fetch is from the url connected to user forwarding port 
-      // before running back end, the webiste will be present on the url 
+        // new url + "api/nodes/get"
+      // before running back end, the website will be present on the forwarding port 
+      // after running url the data is posted on new url + "api/nodes/get"
+
 
 // 6. run backend 
     // open a second terminal: cd to backend folder: cd backend 
     // type: node server.js 
+    // data is posted on new url + "api/nodes/get"
 
 
 
